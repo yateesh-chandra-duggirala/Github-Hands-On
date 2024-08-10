d44892966fc5ce4e3d3d56a372f5edbf3453a43e
= <=>
2024-08-10 16:41:52 +0530

    Including Mavayya's dream

--- hi.py
+++ hi.py
@@ -1,1 +1,5 @@
-print("hi")
+a = int(input("Enter a number : "))
+if a < 100 :
+    print("Major")
+else : 
+    print("Don't joke. you are dead..!")
---
+++ wish.jsx
@@ -0,0 +1,3 @@
+let num = 175
+let statement = "Why Not "
+console.log(statement, num)
